bf5a2f4d0baa94ee82532af8354e751a546617fd
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-05-02 22:55:35 +0000

    update

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://diverse-egret-46.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['media.istockphoto.com', 't3.ftcdn.net', 'benevolent-kangaroo-992.convex.cloud', 'upload.wikimedia.org', 'images.unsplash.com'],
+    },
+};
+
+export default nextConfig;
